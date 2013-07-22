7dfdb515cfc910c1bc87f2ec2f2f1526eadea99c
John Smith <example@stanford.edu>
2013-07-22 21:16:16 +0000

    Nastepna proba

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync("index.html"));
+  response.send(fs.readFileSync('index.html','utf8'));
 });
 
 var port = process.env.PORT || 5000;
